3be4372b7daa633b2349be29db6a67d83c4b68e8
Michael Davis <jrmdd1496@gmail.com>
2022-03-10 20:59:44 -0600

    commit tree 30b3bc5c4c1ae942d7e4e44aa7581f1b6a932156

---
+++ index.js
@@ -0,0 +1,13 @@
+// division function expression
+const divide = function(x,y) {
+    return 2000/100;
+}
+console.log(divide(2000,100));
+
+//square arrow function 
+const square = x => x**x;
+
+// addition arrow function
+ const add = (a,b) => (
+     a+b
+ )
